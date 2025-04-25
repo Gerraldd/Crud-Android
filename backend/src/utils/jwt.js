6396a2524b0ddada2d6bcdf8ba86fac1fd0ae80d
Gerraldd <aaab13415@gmail.com>
2025-04-25 11:21:12 +0700

    first commit

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const accessToken = (data) => {
+    return jwt.sign(
+        { id: data.id, name: data.full_name, email: data.email, role: data.role },
+        process.env.SECRET_KEY_ACCESS_TOKEN,
+        { expiresIn: '12s' }
+    );
+};
+
+const refreshToken = (data) => {
+    return jwt.sign(
+        { id: data.id, name: data.full_name, email: data.email, role: data.role },
+        process.env.SECRET_KEY_REFRESH_TOKEN,
+        { expiresIn: '5m' }
+    );
+};
+
+module.exports = { accessToken, refreshToken };
+
